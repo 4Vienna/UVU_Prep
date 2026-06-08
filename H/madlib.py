5e63b767f03eb39064445d76c7203873c0b28cde
Vienna <43413673+4Vienna@users.noreply.github.com>
2026-06-08 17:03:22 +0000

    H notes

---
+++ H/madlib.py
@@ -0,0 +1,5 @@
+# Vienna LaRose Madlib
+
+# 5 variables that user inputs asking for words
+
+# 1 print statement that uses those words in your madlib
---
+++ H/notes.py
@@ -0,0 +1,22 @@
+# Variables: containers that hold information 
+# variables are created when we lable the space
+
+name = "Ms. LaRose"
+
+# Output => information sent from the computer to the user
+
+print("Hello World!")
+
+# To print a variable, write tne name of the variable with no quotation marks
+print(name)
+
+# input => lets you get information from your user
+
+user = input("What is your name? ")
+
+print("Hello " + user + "!")
+
+# Strings: a collection of letters, numbers or symbols that are surrounded by quotation marks. This is used to seperate instructions for the computer from data being used by the program! 
+
+# Concatenation: puts strings together. + 
+
